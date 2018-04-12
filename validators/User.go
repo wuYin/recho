3a6fb93839bf987e2f1b636b86ba166f92e02862
wuYin <wuYinPost@gmail.com>
2018-04-12 20:54:04 +0800

    修改路由并测试不使用的验证器

--- validators/User.go
+++ validators/User.go
@@ -16,3 +16,11 @@ func (u *User) CheckSession(next echo.HandlerFunc) echo.HandlerFunc {
 		return nil
 	}
 }
+
+func (u *User) UnusedChecker(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		fmt.Println("不会被使用的验证器 :)")
+		next(ctx)
+		return nil
+	}
+}
