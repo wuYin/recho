5e9a3e1155a3f194837caa2e939b168fea52592d
wuYin <wuYinPost@gmail.com>
2018-04-13 13:46:57 +0800

    修改调用的输出信息

--- handlers/User.go
+++ handlers/User.go
@@ -23,7 +23,7 @@ func (u *User) GetUserInfo(ctx echo.Context) {
 	// 业务处理
 	//utils.SendServerError(ctx, nil) // 若服务器处理出错
 
-	fmt.Println("调用 handlers.User.GetUserInfo :)")
+	fmt.Println("调用 handlers.User.GetUserInfo 处理业务逻辑 :)")
 	pike := User{"Robert C. Pike", 62}
 	// 发送响应
 	utils.SendServerSucc(ctx, pike)
